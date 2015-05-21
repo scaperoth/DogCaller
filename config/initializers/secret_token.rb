de7f1f573d21c25c67eedc8c0109af7821e8f3fe
Matt Scaperoth <mscapero@gwu.edu>
2015-05-21 01:53:14 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DogCaller::Application.config.secret_token = '9a4e7f1394d5b992f3f3cb7411f8b633427d00a1022a5447a465496c1cb3609c259725ce4fb5c5f7a88c37eff57240b3a4dcf908ec7b441e397765fac71796a8'
