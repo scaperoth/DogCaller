f4a11cf58b41bd008a211e0b6f043420da727f6f
Matthew Scaperoth <mscapero@gwu.edu>
2015-05-21 11:31:11 -0400

    updating rails

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DogCaller::Application.config.secret_token = '9a4e7f1394d5b992f3f3cb7411f8b633427d00a1022a5447a465496c1cb3609c259725ce4fb5c5f7a88c37eff57240b3a4dcf908ec7b441e397765fac71796a8'
+DogCaller::Application.config.secret_token = '6ce639e0065a72975f00008c6e23371c117e9f6c83058a64b00e7374ce6f5d98f64f4c56c5cd9172e17a1d9e79192f94750a9700d71cf955ea6aeb366a6cf38c'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-DogCaller::Application.config.session_store :cookie_store, key: '_DogCaller_session'
+DogCaller::Application.config.session_store :cookie_store, key: '_dog_caller_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
